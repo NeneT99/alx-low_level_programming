e941192e497701de46a2ba2a915bf79509785ef8
Tendai <anesumwakanheni@gmail.com>
2023-10-28 03:03:03 -0700

    0x09-Static libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -7,8 +7,7 @@ void _puts(char *str)
  *
  * Return: Always 0 (Success)
  */
-int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	    	return (0);
+	puts(str);
+	return (0);
 }
