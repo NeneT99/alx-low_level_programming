de9d81c6fa8574bfa375b00dacfd77207c00e47e
Tendai <anesumwakanheni@gmail.com>
2023-11-06 12:04:16 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -56,16 +56,17 @@ char *_strcpy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	 dog_t *dog;
 	int len1, len2;
 
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
+
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(sizeof(char) * (len2 + 1));
+	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog);
