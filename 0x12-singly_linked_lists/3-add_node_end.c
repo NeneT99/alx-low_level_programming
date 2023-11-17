cdfe4773230487112758d3fde36890877f4576c2
Tendai <anesumwakanheni@gmail.com>
2023-11-17 12:14:23 -0800

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -34,7 +34,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	else
 	{
-		while (current_node-> != NULL)
+		while (current_node->next != NULL)
 			current_node = current_node->next;
 		current_node->next = new_node;
 	}
