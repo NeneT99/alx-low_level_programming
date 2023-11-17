3f76507990c4fb852091a18973da293b0cfe2cbb
Tendai <anesumwakanheni@gmail.com>
2023-11-17 12:21:07 -0800

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,7 +14,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *current_node;
 	size_t n;
-	
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -23,7 +23,6 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	for (n = 0; str[n]; n++)
 		;
-	
 	new_node->len = n;
 	new_node->next = NULL;
 	current_node = *head;
